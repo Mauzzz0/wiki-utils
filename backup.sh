857642c560cc41876f0cdedf7c15d5793a6680fe
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-04-04 19:33:01 +0300

    upd sh files

--- backup.sh
+++ backup.sh
@@ -4,16 +4,13 @@ source '.env'
 
 # Variables
 BACKUP_DIR=./backups
-DB_NAME=$POSTGRES_DB
-DB_USER=$POSTGRES_USER
-DB_PASSWORD=$POSTGRES_PASSWORD
 CONTAINER_NAME=wiki-db-1
 
 # Get current date and time for backup file
 TIMESTAMP=$(date +"%F_%T")
-BACKUP_FILE=$BACKUP_DIR/backup_$DB_NAME_$TIMESTAMP.sql
+BACKUP_FILE=$BACKUP_DIR/backup_$POSTGRES_DB_$TIMESTAMP.sql
 
 # Run pg_dump inside the PostgreSQL container
-docker exec -t $CONTAINER_NAME pg_dump -U $DB_USER $DB_NAME > $BACKUP_FILE
+docker exec -t $CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE
 
 echo "Backup completed: $BACKUP_FILE"
