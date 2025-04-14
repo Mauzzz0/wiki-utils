ba181d792cd0c2b8797029e988a8b201a6eef71d
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-04-14 12:21:55 +0300

    improved backup.sh

--- backup.sh
+++ backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ~/wiki/wiki-utils
-#source ~/wiki/wiki-utils/.env
+source .env
 
 BACKUP_DIR=~/wiki/wiki-utils/backups
 TIMESTAMP=$(date +"%F_%T")
 BACKUP_FILE=$BACKUP_DIR/backup_$POSTGRES_DB_$TIMESTAMP.sql
 
-docker exec --env-file .env -t $POSTGRES_CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE
+docker exec -t $POSTGRES_CONTAINER_NAME pg_dump -U $POSTGRES_USER $POSTGRES_DB > $BACKUP_FILE
 
 npm run backup:notify
 
